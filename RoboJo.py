d1bb800c46f5119a77b95dbefcb30957a1b9b6a4
JoeSubbi <joe.subbiani@gmail.com>
2020-06-23 18:31:05 +0100

    first commit

---
+++ RoboJo.py
@@ -0,0 +1,93 @@
+# bot.py
+import os
+from datetime import date
+import random
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+#load environmnet variables
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+#update connection status
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command(name='pick', help="When followed by a series of space seperated arguments will return a random argument")
+async def pick(ctx, *args):
+    if len(args)==0: await ctx.send('No arguments were given')
+    else:
+        response = random.choice(args)
+        await ctx.send(response)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    #passive behaviour
+    #nice /meme
+    if '69' in message.content or '420' in message.content:
+        response = '***nice!***'
+        await message.channel.send(response)
+
+    #happy birthday
+    if 'happy birthday' in message.content.lower() or 'happy birfday' in message.content.lower():
+        await message.channel.send('**Happy Birthday!** 🎈🎉')
+
+    await bot.process_commands(message)
+
+
+"""
+    #birthdays
+    #list birthdays
+    if message.content[:6] == 'b!list':
+        #read from file
+        with open(GUILD+'birthdays.txt','r') as f:
+            response = f.read()
+            response = ('\n').join(response)
+        
+        #if there are no birthdays, inform the user, else add them to the list
+        if len(response) == 0: await message.channel.send('There are no brithdays, add one using the command: b!add')
+        else: await message.channel.send(response)
+    
+    #add birthdays
+    if message.content[:5] == 'b!add':
+        text = message.content.split(' ')
+        name = text[1]
+        date = text[2]
+
+        with open(GUILD+'birthdays.txt','a') as f:
+            f.write(name+' '+date)
+
+        response = 'Added '+name+'s birthday on '+date
+        await message.channel.send(response)
+    
+    #remove birthdays
+    if message.content[:5] == 'b!rem':
+        name = message.content[5:]
+        if name[0] == ' ': name = name[1:]
+        with open(GUILD+'birthdays.txt','r') as f:
+            text = f.read().split('\n')
+            text = [i.split(' ') for i in text]
+
+            found = False
+            for birthday in text:
+                if birthday[0] == name:
+                    text.pop(birthday)
+                    found = True
+                    break
+            
+            #inform user what is going on
+            if not found: await message.channel.send(name+' Not found')
+            else: await message.channel.send('removed '+name+'s birthday')
+    """
+
+bot.run(TOKEN)
